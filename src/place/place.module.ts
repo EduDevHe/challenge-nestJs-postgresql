b0dfa2774f52da916aa726004cb64bf270c3603a
Eduardo <edu.he.dev@gmail.com>
2024-01-09 14:40:03 -0300

    feat: add prisma service at provides

---
+++ src/place/place.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { PlaceService } from './place.service';
+import { PlaceController } from './place.controller';
+import { PrismaService } from 'src/database/prisma.service';
+@Module({
+  controllers: [PlaceController],
+  providers: [PlaceService, PrismaService],
+})
+export class PlaceModule {}
