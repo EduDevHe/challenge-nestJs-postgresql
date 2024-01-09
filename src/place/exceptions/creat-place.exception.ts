d83c4d318c66cfd76f868f4c2c53a6c2cb8749ad
Eduardo <edu.he.dev@gmail.com>
2024-01-09 20:01:15 -0300

    feat: excpetion to create a place

--- src/place/exceptions/creat-place.exception.ts
+++
@@ -1,6 +0,0 @@
-import { UnprocessableEntityException } from '@nestjs/common';
-export class CreatPlaceException extends UnprocessableEntityException {
-  constructor(message?: string) {
-    super(message || 'All data must be valid');
-  }
-}
