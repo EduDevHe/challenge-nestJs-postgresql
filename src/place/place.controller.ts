7da210ea055b93c0d87f47f920b62fb8d7796f38
Eduardo <edu.he.dev@gmail.com>
2024-01-09 20:05:30 -0300

    feat: route search a place

--- src/place/place.controller.ts
+++ src/place/place.controller.ts
@@ -6,24 +6,25 @@ import {
   Patch,
   Param,
   Delete,
+  Query,
 } from '@nestjs/common';
 import { PlaceService } from './place.service';
 import { CreatePlaceDto } from './dto/create-place.dto';
 // import { UpdatePlaceDto } from './dto/update-place.dto';
 import { HttpException, HttpStatus } from '@nestjs/common';
 import { PlaceNotFoundException } from './exceptions/place-not-found.exception';
-import { CreatPlaceException } from './exceptions/creat-place.exception';
+import { WithoutParameterException } from './exceptions/without-parameter.exception';
 @Controller('place')
 export class PlaceController {
-  constructor(private readonly placeService: PlaceService) { }
+  constructor(private readonly placeService: PlaceService) {}
 
-  @Post()
+  @Post('create')
   async create(@Body() createPlaceDto: CreatePlaceDto) {
     try {
       const place = await this.placeService.create(createPlaceDto);
       return { message: 'place created successfully', place };
     } catch (error) {
-      if (error instanceof CreatPlaceException) {
+      if (error instanceof WithoutParameterException) {
         throw new HttpException(
           {
             status: error.getStatus(),
@@ -97,14 +98,44 @@ export class PlaceController {
         HttpStatus.INTERNAL_SERVER_ERROR,
       );
     }
-
-    // @Patch(':id')
-    // update(@Param('id') id: string, @Body() updatePlaceDto: UpdatePlaceDto) {
-    //   return this.placeService.update(+id, updatePlaceDto);
-    // }
-    //
-    // @Delete(':id')
-    // remove(@Param('id') id: string) {
-    //   return this.placeService.remove(+id);
-    // }
   }
+  @Get()
+  async search(
+    @Query('name') name: string,
+    @Query('city') city: string,
+    @Query('state') state: string,
+  ) {
+    try {
+      return this.placeService.search(name, city, state);
+    } catch (error) {
+      if (error instanceof PlaceNotFoundException) {
+        throw new HttpException(
+          {
+            status: error.getStatus(),
+            error: error.message,
+            details: error.message,
+          },
+          error.getStatus(),
+        );
+      }
+
+      throw new HttpException(
+        {
+          status: HttpStatus.INTERNAL_SERVER_ERROR,
+          error: 'Internal Server Error',
+          details: 'An internal server error occurred. Please try again later.',
+        },
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updatePlaceDto: UpdatePlaceDto) {
+  //   return this.placeService.update(+id, updatePlaceDto);
+  // }
+  //
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.placeService.remove(+id);
+  // }
+}
